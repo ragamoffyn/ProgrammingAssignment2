8d4e8e9e5feefa23e8ca6122bda2a40eae6eb360
ragamoffyn <screenaccount@gmail.com>
2015-02-21 12:16:48 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates and stores a list of functions
+## for creating and retrieving a matrix and its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ## Find x in the parent environment and assign it the value in y.
+        setdata <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        
+        ## Return the value of x, which is passed down from
+        ## cacheSolve to makeCacheMatrix to getdata.
+        getdata <- function() x
 
+        ## Look for i in a parent environment, and use it to cache
+        ## the inverse matrix in inv passed from cacheSolve.
+        setinv <- function(inv) i <<- inv
+
+        ## Look for i in a parent environment and return it.
+        ## The value of i will be either null or, if setinv has been called,
+        ## a cached inverse matrix.
+        getinv <- function() i
+
+        ## Return of list of the functions.
+        list(setdata = setdata, getdata = getdata,
+             setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## This function calculates the inverse of the matrix
+## created in or retreived from makeCacheMatrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        ## Create a placeholder for the inverse matrix.
+        i <- NULL
+        
+        ## Call getinv in makeCacheMatrix to get any cached values.
+        i <- x$getinv()
+        
+        ## If i is not null, that means there is a cached value,
+        ## so just return that and then quit.
+        if(!is.null(i)) {
+                print("getting cached data")
+                return(i)
+        }
+
+        ## Otherwise, if i is null, then there are no cached values.
+        ## In that case, call getdata in makeCacheMatrix to assign
+        ## any matrix values currently in x to data.
+        data <- x$getdata()
+        
+        ## Calculate the inverse of the matrix in data and assign it to i.
+        i <- solve(data)
+
+        ## Cache the inverse matrix now stored in i by calling
+        ## setinv in makeCacheMatrix.
+        x$setinv(i)
+
+        ## Return the inverse matrix.
+        i
 }
